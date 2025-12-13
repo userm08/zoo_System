8ee3cf537fd01cccf834707f360cb1f4c37f1ef3
userm08 <kosamannurss7@gmail.com>
2025-12-13 12:50:14 +0500

    animal

---
+++ src/Animal.java
@@ -0,0 +1,28 @@
+public class Animal {
+    private String name;
+    private String species;
+    private int age;
+
+    public Animal(String name, String species, int age) {
+        this.name = name;
+        this.species = species;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSpecies() {
+        return species;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
+
---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Animal a1 = new Animal("Leo", "Lion", 5);
+        Animal a2 = new Animal("Max", "Tiger", 3);
+
+        Zookeeper zookeeper = new Zookeeper("John", 7);
+        Zoo zoo = new Zoo("City Zoo", 2);
+
+        System.out.println(a1);
+        System.out.println(a2);
+        System.out.println(zookeeper);
+        System.out.println(zoo);
+
+        if (a1.getAge() > a2.getAge()) {
+            System.out.println(a1.getName() + " is older than " + a2.getName());
+        } else {
+            System.out.println(a2.getName() + " is older than " + a1.getName());
+        }
+    }
+}
